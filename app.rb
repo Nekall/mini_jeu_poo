ecbdf61bba6fb5b6bbaa4407d7be01fd4e4e6190
Neka <nekoanchor@yahoo.fr>
2021-01-26 12:41:07 +0100

    MAJ v0.1

--- app.rb
+++ app.rb
@@ -1,11 +1,11 @@
-#require 'pry'
+require 'pry'
 
 require_relative 'lib/game'
 require_relative 'lib/player'
-
+#Creation des joueurs
 player1 = Player.new("Josiane")
 player2 = Player.new("José")
-
+#Combats jusqu'a la mort d'un des deux joueurs
 while(player1.life_points != 0 or player2.life_points != 0)
   puts "Voici l'état de chaque joueur :"
   player1.show_state
