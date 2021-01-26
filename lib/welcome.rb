e171149fed870cd53abd89f9c88b40ef275b7586
Neka <nekoanchor@yahoo.fr>
2021-01-26 19:52:22 +0100

    ajout graphics

---
+++ lib/welcome.rb
@@ -0,0 +1,60 @@
+require 'pry'
+
+puts "                     CECI EST UN TITRE"
+puts "              />"
+puts " (           //-------------------------------------("
+puts "(*)OXOXOXOXO(*>======================================\\"
+puts " (           \\\\---------------------------------------)"
+puts "              \\>"
+puts " "
+puts ""
+print " B                       \r"
+sleep(0.2)
+print " Bi                      \r"
+sleep(0.2)
+print " Bie                     \r"
+sleep(0.2)
+print " Bien                    \r"
+sleep(0.2)
+print " Bienv                   \r"
+sleep(0.2)
+print " Bienve                  \r"
+sleep(0.2)
+print " Bienven                 \r"
+sleep(0.2)
+print " Bienvenu                \r"
+sleep(0.2)
+print " Bienvenue               \r"
+sleep(0.2)
+print " Bienvenue a             \r"
+sleep(0.2)
+print " Bienvenue av            \r"
+sleep(0.2)
+print " Bienvenue ave           \r"
+sleep(0.2)
+print " Bienvenue aven          \r"
+sleep(0.2)
+print " Bienvenue avent         \r"
+sleep(0.2)
+print " Bienvenue aventu        \r"
+sleep(0.2)
+print " Bienvenue aventur       \r"
+sleep(0.2)
+print " Bienvenue aventuri      \r"
+sleep(0.2)
+print " Bienvenue aventurie     \r"
+sleep(0.2)
+print " Bienvenue aventurier    \r"
+sleep(0.2)
+print " Bienvenue aventurier⸱   \r"
+sleep(0.2)
+print " Bienvenue aventurier⸱è  \r"
+sleep(0.2)
+print " Bienvenue aventurier⸱èr \r"
+sleep(0.2)
+print " Bienvenue aventurier⸱ère\r"
+sleep(0.5)
+puts " "
+print "Tu dois sortir vivant⸱e de ce Donjon !"
+puts " "
+print "Mais tout d'abord, comment te fais-tu appeler ?"
