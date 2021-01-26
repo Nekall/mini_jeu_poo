f93b75ad0f7c2a2e189b112b3f3e6b9fb82d5f79
Neka <nekoanchor@yahoo.fr>
2021-01-26 15:02:24 +0100

    modif players

--- lib/player.rb
+++ lib/player.rb
@@ -5,9 +5,9 @@ class Player
   attr_accessor :life_points
   attr_accessor :attacked_player
 
-#Métode de creation d'un joueur
-  def initialize(name_to_save)
-    @name = name_to_save
+#Métode de creation d'un joueur avec 10 points de vie
+  def initialize(name)
+    @name = name
     @life_points = 10
   end
 #Méthode pour affichez les stats d'un joueur
@@ -34,6 +34,55 @@ class Player
 #Randomizer dice
   def compute_damage
     return rand(1..6)
+  end
+end
+
+class HumanPlayer < Player
+  attr_accessor :weapon_level
+  attr_accessor :life_points
+
+#Métode de creation d'un joueur humain avec 100 points de vie et une arme de lvl1
+  def initialize(name)
+    @life_points = (@life_points.to_i + 90)
+    @weapon_level = 1
+
+    super(name)
+  end
+#Méthode pour affichez les stats d'un joueur humain
+  def show_state
+    puts "#{@name} a #{@life_points} points de vie et une arme de niveau #{@weapon_level}"
+  end
+#Damage avec boost du lvl de l'arme
+  def compute_damage
+    rand(1..6) * @weapon_level
+  end
+#Méthode de rechercher d'une nouvelle arme
+  def search_weapon
+    weapon_lvl_found = rand(1..6)
+    puts "Tu as trouvé une arme de niveau #{weapon_lvl_found}"
+    if(weapon_lvl_found > @weapon_level)
+      then @weapon_level = weapon_lvl_found
+           puts "Youhou ! elle est meilleure que ton arme actuelle : tu la prends."
+      else puts "M@*#$... elle n'est pas mieux que ton arme actuelle..."
+    end
+  end
+#Méthode de rechercher d'un pack de vie
+  def search_health_pack
+    health_kit_found = rand(1..6)
+    if(health_kit_found == 1) then puts "Tu n'as rien trouvé..."
+    end
+    if(health_kit_found >= 2 and health_kit_found <= 5)
+      then puts "Bravo, tu as trouvé un pack de +50 points de vie !"
+        @life_points = @life_points + 50
+        if(@life_points > 100) then @life_points = 100
+        end
+    end
+    if(health_kit_found == 6)
+      then puts "Waow, tu as trouvé un pack de +80 points de vie !"
+        @life_points = @life_points + 80
+        if(@life_points > 100) then @life_points = 100
+        end
+    end
   end
 
 end
